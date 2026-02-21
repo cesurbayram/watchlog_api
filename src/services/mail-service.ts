0637adbfd6416b625eb3d6ed98392db43b0d21b3
mstfakks <msfa.akkas@gmail.com>
2026-02-21 19:05:52 +0300

    feat: Refactor mail service to use dynamic SMTP configuration from database

--- src/services/mail-service.ts
+++ src/services/mail-service.ts
@@ -1,14 +1,31 @@
 import nodemailer from "nodemailer";
+import { dbPool } from "../config/db";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  auth: {
-    user: process.env.FROM_EMAIL,
-    pass: process.env.FROM_PASSWORD,
-  },
-});
+interface TransporterConfig {
+  smtp_host: string;
+  smtp_port: number;
+  smtp_user: string;
+  smtp_password: string;
+}
 
+const createDynamicTransporter = ({ smtp_host, smtp_port, smtp_user, smtp_password }: TransporterConfig) => {
+  return nodemailer.createTransport({
+    // host: "smtp.gmail.com",
+    // port: 587,
+    // secure: true,
+    // auth: {
+    //   user: process.env.FROM_EMAIL,
+    //   pass: process.env.FROM_PASSWORD,
+    // },
+    host: smtp_host,
+    port: smtp_port,
+    //secure: true,
+    auth: {
+      user: smtp_user,
+      pass: smtp_password,
+    },
+  });
+};
 interface SendMailProps {
   email: string;
   message: string;
@@ -16,8 +33,14 @@ interface SendMailProps {
 }
 
 export const sendMail = async ({ email, message, subject }: SendMailProps): Promise<void> => {
+  const smtpConfigDbRes = await dbPool.query<TransporterConfig>(`SELECT smtp_host, smtp_port, smtp_user, smtp_password FROM company`);
+
+  const smtpConfigData = smtpConfigDbRes.rows[0];
+
+  const transporter = createDynamicTransporter(smtpConfigData);
+
   await transporter.sendMail({
-    from: process.env.FROM_EMAIL,
+    from: smtpConfigData.smtp_user,
     to: email,
     subject: subject,
     text: message,
