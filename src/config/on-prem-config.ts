9440d1cbcab1a924f8ee8e8998e91008add82fa3
Cesur Bayram <cesr.bayram@gmail.com>
2026-04-03 12:32:41 +0300

    changed some function

--- src/config/on-prem-config.ts
+++ src/config/on-prem-config.ts
@@ -12,10 +12,9 @@ export const ON_PREM_SECRET =
 export const ON_PREM_NOTIFICATION_SERVER_URL = "http://localhost:4001";
 export const ON_PREM_MOTO_SERVER_URL = "http://localhost:8082";
 
-export const ON_PREM_WATCHLOG_BASE_DIR = "/Users/cesur/Watchlog/UI";
-export const ON_PREM_WATCHLOG_WELD_DIR = "/Users/cesur/Watchlog/Weld";
-export const ON_PREM_WATCHLOG_BACKUP_DIR = "/Users/cesur/Watchlog/Backup";
-export const ON_PREM_WATCHLOG_ALARM_DETAILS_DIR =
-  "/Users/cesur/Watchlog/AlarmDetails";
+export const ON_PREM_WATCHLOG_BASE_DIR = "C:/WatchLog/UI";
+export const ON_PREM_WATCHLOG_WELD_DIR = "C:/WatchLog/Weld";
+export const ON_PREM_WATCHLOG_BACKUP_DIR = "C:/WatchLog/Backup";
+export const ON_PREM_WATCHLOG_ALARM_DETAILS_DIR = "C:/WatchLog/AlarmDetails";
 
 export const ON_PREM_MONGO_URI = "mongodb://localhost:27017/watchlog";
